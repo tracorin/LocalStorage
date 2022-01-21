aca874ae365d19fc1fdc495ed0a94850ec510cf5
Yurii Zakharchyn <yurii.zakharchyn.personal@gmail.com>
2022-01-21 21:31:24 +0200

    local storage methods

---
+++ LocalStorage/index.js
@@ -0,0 +1,11 @@
+export const setToken = (token) => {
+  localStorage.setItem("token", JSON.stringify(token));
+};
+
+export const getToken = () => {
+  return JSON.parse(localStorage.getItem("token"));
+};
+
+export const removeToken = () => {
+  localStorage.removeItem("token");
+};
